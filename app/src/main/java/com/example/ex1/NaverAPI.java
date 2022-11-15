41a2b3569878de36817a4d78f976e3d121ac26dc
tlffur <yunho000331@naver.com>
2022-11-16 03:21:37 +0900

    메인창 변경

--- app/src/main/java/com/example/ex1/NaverAPI.java
+++ app/src/main/java/com/example/ex1/NaverAPI.java
@@ -46,41 +46,41 @@ public class NaverAPI {
 
         }
     }
-    public static URL dbdkfdpf(String query, int start) {
-        String clientId = "7GrEUKj3HB3eKIyOvolk";//애플리케이션 클라이언트 아이디값";
-        String clientSecret = "zKpDPpuPQ6";//애플리케이션 클라이언트 시크릿값";
-        HttpURLConnection con;
-        try {
-            String text = URLEncoder.encode(query, "UTF-8");
-            String apiURL = BASE_URL + text; // json 결과
-            //String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text; // xml 결과
-            apiURL += "&start" + start;
-
-            URL url = new URL(apiURL);
-            con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("X-Naver-Client-Id", clientId);
-            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
-            int responseCode = con.getResponseCode();
-            BufferedReader br;
-            if (responseCode == 200) { // 정상 호출
-                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            } else {  // 에러 발생
-                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-            }
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = br.readLine()) != null) {
-                response.append(inputLine);
-            }
-            br.close();
-            System.out.println(response.toString());
-            return url;
-        } catch (Exception e) {
-            System.out.println("error..............." + e.toString());
-            return null;
-
-        }
-    }
+//    public static URL dbdkfdpf(String query, int start) {
+//        String clientId = "7GrEUKj3HB3eKIyOvolk";//애플리케이션 클라이언트 아이디값";
+//        String clientSecret = "zKpDPpuPQ6";//애플리케이션 클라이언트 시크릿값";
+//        HttpURLConnection con;
+//        try {
+//            String text = URLEncoder.encode(query, "UTF-8");
+//            String apiURL = BASE_URL + text; // json 결과
+//            //String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text; // xml 결과
+//            apiURL += "&start" + start;
+//
+//            URL url = new URL(apiURL);
+//            con = (HttpURLConnection) url.openConnection();
+//            con.setRequestMethod("GET");
+//            con.setRequestProperty("X-Naver-Client-Id", clientId);
+//            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+//            int responseCode = con.getResponseCode();
+//            BufferedReader br;
+//            if (responseCode == 200) { // 정상 호출
+//                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+//            } else {  // 에러 발생
+//                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+//            }
+//            String inputLine;
+//            StringBuffer response = new StringBuffer();
+//            while ((inputLine = br.readLine()) != null) {
+//                response.append(inputLine);
+//            }
+//            br.close();
+//            System.out.println(response.toString());
+//            return url;
+//        } catch (Exception e) {
+//            System.out.println("error..............." + e.toString());
+//            return null;
+//
+//        }
+//    }
 
 }
